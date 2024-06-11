e259c10146ab42a75b0171dae3b0a1ecc7441a6d
erviejohn <erviejohn.villareal79@gmail.com>
2024-06-11 15:52:39 +0800

    try 3

--- communication2.py
+++ communication2.py
@@ -1,8 +1,11 @@
 import serial
 import time
 
+global ser
+
 def establish_serial_connection(port):
     try:
+        global ser
         ser = serial.Serial(port, baudrate = 115200, timeout=1)
         print(f"Serial Connection established on {port}")
         return ser
@@ -11,7 +14,8 @@ def establish_serial_connection(port):
         return None
 
 
-def main(command):
+def init_serial():
+    global ser
     serial_ports = ['/dev/ttyACM0','/dev/ttyUSB0','/dev/ttyUSB1','/dev/ttyUSB2','/dev/ttyUSB3','/dev/ttyS0','/dev/ttyTHS1','/dev/ttyTHS2']
 
     # serial_ports = ['COM1','COM2', 'COM3', 'COM4']
@@ -19,14 +23,23 @@ def main(command):
     for port in serial_ports:
         ser = establish_serial_connection(port)
         if ser:
-            ser.write(command.encode())
-            ser.close()
+            ser.open()
             break
 
     if not ser:
         print("Failed to establish connection!")
-        return   
+        return
 
+def main(command):
+    global ser
+    ser.close()
+    ser.open()
+    command = command.upper()
+    ser.write(command.encode())
+    time.sleep(5)
+    print(f'Sent command: {command}')
+       
+init_serial()
 while True:
     command = input("Enter command: ")
     main(command)
