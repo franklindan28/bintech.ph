2af8e77db42938c964ecb0e8cb091bfc4bfa0137
erviejohn <erviejohn.villareal79@gmail.com>
2024-06-11 16:12:20 +0800

    try sending command 1

--- communication3.py
+++ communication3.py
@@ -4,5 +4,8 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', baudrate = 115200, timeout=1)
 
 if ser.is_open:
-    ser.write("OPEN".encode())
+    user_input = "OPEN".upper()
+    ser.write(user_input.encode())
+    print(f'Sent command: {user_input}')
+    time.sleep(3)
 
