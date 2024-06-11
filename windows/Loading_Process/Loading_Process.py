ba6e18e7aadf2c1fcdd0146f913b18ee74cc538f
erviejohn <erviejohn.villareal79@gmail.com>
2024-06-11 15:28:31 +0800

    Added ser.close and open on sending command

--- windows/Loading_Process/Loading_Process.py
+++ windows/Loading_Process/Loading_Process.py
@@ -236,12 +236,15 @@ class Loading_Process(QMainWindow):
 
     def sendToArduino(self, detectionResult):
         try:
+            self.ser.close()
+            self.ser.open()
             # while True:
             #     user_input = input("Enter Command (start): ").upper()
             #     ser.write(user_input.encode())
             #     print(f'Sent command: {user_input}')
             detectionResult = detectionResult.upper()
             self.ser.write(detectionResult.encode())
+            time.sleep(3)  #at least wait for 2s
             print(f'Sent command: {detectionResult}')
             
         except KeyboardInterrupt:
