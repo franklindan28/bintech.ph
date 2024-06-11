2b71f669bc45db4b0c6a2842b7a32f87fc0f4032
erviejohn <erviejohn.villareal79@gmail.com>
2024-06-11 16:45:48 +0800

    Added to function

--- communication3.py
+++ communication3.py
@@ -4,17 +4,15 @@ import serial
 ser = serial.Serial('/dev/ttyACM0', baudrate = 115200, timeout=1)
 
 #while True:
-user_input = input("Enter Command: ").upper()
-print("user_input_encode: ", user_input.encode())
-ser.write(user_input.encode())
-ser.flush()
-time.sleep(0.1)
-print(f'Sent command: {user_input}')
+def send_command():
+    user_input = input("Enter Command: ").upper()
+    print("user_input_encode: ", user_input.encode())
+    ser.write(user_input.encode())
+    ser.flush()
+    time.sleep(0.1)
+    print(f'Sent command: {user_input}')
 
-user_input = input("Enter Command: ").upper()
-print("user_input_encode: ", user_input.encode())
-ser.write(user_input.encode())
-ser.flush()
-time.sleep(0.1)
-print(f'Sent command: {user_input}')
+send_command()
+send_command()
+send_command()
 
