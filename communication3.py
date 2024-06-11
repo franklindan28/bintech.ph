96c393be131b6bee62cb08ae57c34278dba7730d
franklindan28 <franklindanas@gmail.com>
2024-06-11 16:29:56 +0800

    Added sending 2 commands

--- communication3.py
+++ communication3.py
@@ -3,9 +3,18 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', baudrate = 115200, timeout=1)
 
-if ser.is_open:
-    user_input = "OPEN".upper()
-    ser.write(user_input.encode())
-    print(f'Sent command: {user_input}')
-    time.sleep(3)
+#while True:
+user_input = input("Enter Command: ").upper()
+print("user_input_encode: ", user_input.encode())
+ser.write(user_input.encode())
+ser.flush()
+time.sleep(0.1)
+print(f'Sent command: {user_input}')
+
+user_input = input("Enter Command: ").upper()
+print("user_input_encode: ", user_input.encode())
+ser.write(user_input.encode())
+ser.flush()
+time.sleep(0.1)
+print(f'Sent command: {user_input}')
 
